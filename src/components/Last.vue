44ab3ad07f3fe399f3e58bcccdffaee345b24cc5
Önsel AYDIN <onselaydin@gmail.com>
2021-01-05 20:36:53 +0300

    general

--- src/components/Last.vue
+++ src/components/Last.vue
@@ -1,27 +1,29 @@
 <template>
     <div class="container">
-       
+
             <p><br></p>
-            
+
             <img alt="Qries" v-bind:src="article.image" width="150" height="70" v-if="article.image!=''">
             <p><br></p>
             <div class="form-group">
                 <h1>
                     <label>{{article.title}}</label>
                 </h1>
-            
+
             <!-- <input v-model="article.title" class="form-control" placeholder="title" /> -->
             </div>
             <div class="form-group">
                <div v-html="article.content"></div>
             </div>
-            
+
             <div class="form-group">
                 <a :href="article.url">{{article.url}}</a>
             </div>
-            
+
             <div class="form-group">
-                <label>{{article.arttypeDetail[0].title}}</label>
+                <div v-for="a in article.arttypeDetail">
+                  {{a.title}}
+                </div>
             </div>
             <div class="form-group">
                 <label>{{article.dates}}</label>
@@ -33,11 +35,11 @@ import axios from "axios";
 export default {
   created() {
     this.getData();
-    
+
   },
   data() {
     return {
-      
+
       article: {
           title: "",
           content: "",
