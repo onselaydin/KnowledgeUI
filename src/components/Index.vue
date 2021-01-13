c723af923baf713c647822d8a9ac71a92430da09
Önsel AYDIN <onselaydin@gmail.com>
2021-01-13 17:17:14 +0300

    div change fixed

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,69 +1,60 @@
 <template>
   <div class="container">
+    <section class="mb-4">
+      <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-dark teal lighten-2 mb-4">
+          <!-- Navbar brand -->
+          <a class="navbar-brand" href="#"></a>
 
-  <nav class="navbar navbar-expand-lg navbar-dark teal lighten-2 mb-4">
-
-  <!-- Navbar brand -->
-  <a class="navbar-brand" href="#"></a>
-
-  <!-- Collapsible content -->
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-    <!-- Search form -->
-    <form class="form-inline ml-auto">
-      <div class="md-form my-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Ara" aria-label="Ara" v-on:change="findArticle($event)">
-      </div>
-    </form>
-  </div>
-  <!-- Collapsible content -->
-
-</nav>
-
-
-    <div class="row">
-      <div v-for="article in articleList" :key="article.Id">
-        <div class="card-expansion">
-          <md-card>
-            <md-card-media>
-              <img :src="article.image" alt="okipu.net" style="width:310px;height:140px;"/>
-            </md-card-media>
-
-            <md-card-header>
-              <div class="md-title">{{ article.title }}</div>
-              <div class="md-subhead">
-                <a :href="article.url">Ayrıca...</a>
+          <!-- Collapsible content -->
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Search form -->
+            <form class="form-inline ml-auto">
+              <div class="md-form my-0">
+                <input
+                  class="form-control mr-sm-2"
+                  type="text"
+                  placeholder="Ara"
+                  aria-label="Ara"
+                  v-on:change="findArticle($event)"
+                />
               </div>
-            </md-card-header>
-            <md-card-actions md-alignment="space-between">
-                <div>
-                  <md-button @click="detail(article.id)">Detay</md-button>
-                </div>
-            </md-card-actions>
-            
-            <!-- <md-card-expand>
-              <md-card-actions md-alignment="space-between">
-                <div>
-                  <md-button>Detay...</md-button>
-                </div>
+            </form>
+          </div>
+          <!-- Collapsible content -->
+        </nav>
+      </div>
 
-                <md-card-expand-trigger>
-                  <md-button class="md-icon-button">
-                    <md-icon>keyboard_arrow_down</md-icon>
-                  </md-button>
-                </md-card-expand-trigger>
-              </md-card-actions>
+      <div class="row">
+        <div class="col-md-9 mb-md-0 mb-5">
+          <div v-for="article in articleList" :key="article.Id">
+            <div class="card-expansion">
+              <md-card>
+                <md-card-media>
+                  <img
+                    :src="article.image"
+                    alt="okipu.net"
+                    style="width: 310px; height: 140px"
+                  />
+                </md-card-media>
 
-              <md-card-expand-content>
-                <md-card-content>
-                  <div v-html="article.content"></div>
-                </md-card-content>
-              </md-card-expand-content>
-            </md-card-expand> -->
-          </md-card>
+                <md-card-header>
+                  <div class="md-title">{{ article.title }}</div>
+                  <div class="md-subhead">
+                    <a :href="article.url">Ayrıca...</a>
+                  </div>
+                </md-card-header>
+                <md-card-actions md-alignment="space-between">
+                  <div>
+                    <md-button @click="detail(article.id)">Detay</md-button>
+                  </div>
+                </md-card-actions>
+              </md-card>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -80,9 +71,9 @@ export default {
     };
   },
   methods: {
-    detail(Id){
+    detail(Id) {
       this.$store.state.articleId = Id;
-      this.$router.push({name:'articledetail'});
+      this.$router.push({ name: "articledetail" });
     },
     getData() {
       axios
@@ -93,19 +84,19 @@ export default {
         })
         .catch((e) => console.log(e));
     },
-    findArticle(e){
+    findArticle(e) {
       //console.log(e.target.value);
       axios
-        .get("articlecustom/FindArticle/",{
-          params:{
-            ArticleName: e.target.value
-          }
+        .get("articlecustom/FindArticle/", {
+          params: {
+            ArticleName: e.target.value,
+          },
         })
         .then((response) => {
           this.articleList = response.data;
         })
         .catch((e) => console.log(e));
-    }
+    },
   },
 };
 </script>
@@ -122,5 +113,4 @@ export default {
   display: inline-block;
   vertical-align: top;
 }
-
 </style>
