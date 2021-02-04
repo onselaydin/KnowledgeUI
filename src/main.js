231fcbff03325874e373467f6dd5945f9418c00c
Önsel AYDIN <onselaydin@gmail.com>
2021-02-04 12:53:06 +0300

    test

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ import BootstrapVue from 'bootstrap-vue'
 
 axios.defaults.baseURL = "http://139.59.215.230/api";
 //axios.defaults.baseURL = "http://localhost:5000/api";
-axios.defaults.headers.common["Authorization"] = 'Bearer '+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJyb2xlIjoiQWRtaW4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiJWMy4xIiwibmJmIjoxNjEyMjExNzU3LCJleHAiOjE2MTIzODQ1NTcsImlhdCI6MTYxMjIxMTc1N30.eIgXtbsvn_mEINz5vbhLWebvk7cKnkQPp9mCPNk3Wcw' ;
+axios.defaults.headers.common["Authorization"] = 'Bearer '+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJyb2xlIjoiQWRtaW4iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiJWMy4xIiwibmJmIjoxNjEyNDMyMzQwLCJleHAiOjE2MTI2MDUxNDAsImlhdCI6MTYxMjQzMjM0MH0.hvmZXUON18_wtVNAwOkZIWWxMkNW5JADeGqJcfpRQH8' ;
 axios.defaults.headers.get["Accepts"] = "application/json";  //sadece json kabul
 
 
